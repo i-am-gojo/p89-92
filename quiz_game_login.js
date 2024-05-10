368a6e46f5a05f68adfbcd379bff9ea0c603f3c8
i-am-gojo <161028156+i-am-gojo@users.noreply.github.com>
2024-05-10 18:18:25 +0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,9 @@
+
+function adduser(){
+player1=document.getElementById("player1name").value;
+player2=document.getElementById("player2name").value;
+
+localStorage.setItem("player1",player1)
+localStorage.setItem("player2",player2)
+window.location="game_page.html"
+}
